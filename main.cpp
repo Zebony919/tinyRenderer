6e7be02d9e6bf4dfc2646824463b551927ada316
Xavier Miller <Zebony9@gmail.com>
2026-08-09 13:25:06 +1200

    feat: Can draw lines :)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "tgaimage.h"
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
@@ -5,6 +6,14 @@ constexpr TGAColor green   = {  0, 255,   0, 255};
 constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
+
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+    for (float t = 0; t < 1.; t += 0.02) {
+        int x = std::round( ax + (bx - ax) * t );
+        int y = std::round( ay + (by - ay) * t );
+        framebuffer.set(x, y, color);
+    }
+}
 
 int main(int argc, char** argv) {
     constexpr int width  = 64;
@@ -15,12 +24,14 @@ int main(int argc, char** argv) {
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
 
-    for (int i = -3; i <= 3; i++)
-        for (int j = -3; j <= 3; j++)
-            framebuffer.set(ax+i, ay+j, red);
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
 
-    framebuffer.set(bx, by, blue);
-    framebuffer.set(cx, cy, green);
+    framebuffer.set(ax, ay, white);
+    framebuffer.set(bx, by, white);
+    framebuffer.set(cx, cy, white);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
