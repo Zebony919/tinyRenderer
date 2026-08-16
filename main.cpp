c754b6e715863ecd70431a077e416be9d2999196
Xavier Miller <Zebony9@gmail.com>
2026-08-16 18:08:47 +1200

    feat: Can load models and render them

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <cmath>
+#include <tuple>
+#include "geometry.h"
+#include "model.h"
 #include "tgaimage.h"
+
+constexpr int width  = 800;
+constexpr int height = 800;
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
@@ -39,27 +45,39 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
     }
 }
 
+// Learn
+std::tuple<int, int> project(vec3 v) {
+    return { (v.x + 1.) * width/2,
+             (v.y + 1.) * height/2};
+}
+// Learn End
+
 int main(int argc, char** argv) {
-    constexpr int width  = 64;
-    constexpr int height = 64;
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
+        return 1; 
+    }
+
+    Model model(argv[1]);
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    int ax =  7, ay =  3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
-    int dx = 10, dy = 60;
-    int ex = 60, ey = 10;
+    // Learn
+    for (int i = 0; i < model.nfaces(); i++) {
+        auto [ax, ay] = project(model.vert(i, 0).xyz());
+        auto [bx, by] = project(model.vert(i, 1).xyz());
+        auto [cx, cy] = project(model.vert(i, 2).xyz());
 
-    line(ax, ay, bx, by, framebuffer, blue);
-    line(cx, cy, bx, by, framebuffer, green);
-    line(ax, ay, cx, cy, framebuffer, red);
-    line(dx, dy, ex, ey, framebuffer, blue);
+        line(ax, ay, bx, by, framebuffer, red);
+        line(bx, by, cx, cy, framebuffer, red);
+        line(cx, cy, ax, ay, framebuffer, red);
+    }
 
-    framebuffer.set(ax, ay, white);
-    framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
-    framebuffer.set(dx, dy, white);
-    framebuffer.set(ex, ey, white);
+    for (int i = 0; i < model.nverts(); i++) {
+        vec3 v = model.vert(i).xyz();
+        auto [x, y] = project(v);
+        framebuffer.set(x, y, white);
+    }
+    // Learn End
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
--- model.h
+++ model.h
@@ -24,4 +24,3 @@ public:
     const TGAImage& specular() const;
 
 };
-
