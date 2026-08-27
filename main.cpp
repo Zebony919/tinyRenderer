f7b5c8d79e17eff46c98e323324fc8dd75b8eaa0
zebony919 <zebony9@gmail.com>
2026-08-27 13:03:15 +1200

    feat: Draw triangles using bounding box

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include "model.h"
 #include "tgaimage.h"
 
-constexpr int width  = 100;
-constexpr int height = 100;
+constexpr int width  = 800;
+constexpr int height = 800;
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
@@ -45,6 +45,11 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
     }
 }
 
+double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy) {
+    return 0.5 * ((by - ay)*(bx + ax) + (cy - by)*(cx + bx) + (ay - cy)*(ax + cx));
+    // Learn this math
+}
+
 void drawTriangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
     // Vertices a, b, and c are now sorted in ascending order so point a is lowest in terms of the y-axis
     if (ay > by) {std::swap(ax, bx); std::swap(ay, by);}
@@ -53,6 +58,28 @@ void drawTriangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &fram
     // Boundary A is the line from the lowest point to the highest point
     // So in this case that would be from point a (lowest) to point c (highest)
 
+    int bbminx = std::min(std::min(ax, bx), cx); // Lowest vertice
+    int bbminy = std::min(std::min(ay, by), cy);
+    int bbmaxx = std::max(std::max(ax, bx), cx); // Highest vertice
+    int bbmaxy = std::max(std::max(ay, by), cy);
+    double total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
+
+    #pragma omp parallel for // Allows for parallel iteration where the compiler can split up the for loop amongst multiple cores
+    for (int x = bbminx; x <= bbmaxx; x++) {
+        for (int y = bbminy; y <= bbmaxy; y++) {
+            double alpha = signed_triangle_area(x, y, bx, by, cx, cy) / total_area;
+            double beta = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
+            double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
+
+            if (alpha < 0 || beta < 0 || gamma < 0) continue; // Negative barycentric coordinate
+            // If its negative then the pixel is outside the triangle bound
+
+            framebuffer.set(x, y, color);
+        }
+    }
+    // Able to draw squares by iterating from the bottom left corner/bottom vertice to the upper right corner/upper vertice
+
+    /* 
     int totalHeight = cy - ay;
 
     if (ay != by) { // Meaning that point a and b aren't horizontally parallel where there is no "bottom half" in a sense
@@ -61,9 +88,6 @@ void drawTriangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &fram
         for (int y = ay; y <= by; y++) {
             int x1 = ax + ((cx - ax) * (y - ay)) / totalHeight;
             int x2 = ax + ((bx - ax) * (y - ay)) / segmentHeight;
-            framebuffer.set(x1, y, color); // Boundary A line
-            framebuffer.set(x2, y, color); // Boundary B line
-            // Both lines are being drawn simultaneously
 
             for (int x = std::min(x1, x2); x < std::max(x1, x2); x++) {
                 framebuffer.set(x, y, color);
@@ -73,7 +97,7 @@ void drawTriangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &fram
         }
     }
 
-    if (by != cy) { // Meaning point b and c aren't horizontal
+    if (by != cy) { // Meaning point b and c aren't horizontally parallel
         int segmentHeight = cy - by;
 
         for (int y = by; y <= cy; y++) {
@@ -86,6 +110,7 @@ void drawTriangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &fram
         }
     }
     // This loop is doing practically the same thing as the other but for the top half of the triangle
+    */
 }
 
 /* 
@@ -99,35 +124,31 @@ std::tuple<int, int> project(vec3 v) {
 }
 
 int main(int argc, char** argv) {
-    /*
+    
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
         return 1; 
     }
-    */
+    
 
-    // Model model(argv[1]);
+    Model model(argv[1]);
     TGAImage framebuffer(width, height, TGAImage::RGB);
-
-    drawTriangle(2, 5, 20, 24, 50, 21, framebuffer, red);
-    drawTriangle(10, 20, 54, 37, 88, 12, framebuffer, blue);
-    drawTriangle(20, 5, 34, 77, 18, 92, framebuffer, green);
 
     /*  
         For each face, project its 3 vertices from 3D model space 
         to 2D screen space, then draw the 3 edges connecting them.
+    */
     
     for (int i = 0; i < model.nfaces(); i++) {
         auto [ax, ay] = project(model.vert(i, 0).xyz());
         auto [bx, by] = project(model.vert(i, 1).xyz());
         auto [cx, cy] = project(model.vert(i, 2).xyz());
 
-        line(ax, ay, bx, by, framebuffer, red);
-        line(bx, by, cx, cy, framebuffer, red);
-        line(cx, cy, ax, ay, framebuffer, red);
+        TGAColor rnd;
+        for (int c = 0; c <= 3; c++) rnd[c] = std::rand()%255;
+        drawTriangle(ax, ay, bx, by, cx, cy, framebuffer, rnd);
     }
 
-    */
 
 
     /*
